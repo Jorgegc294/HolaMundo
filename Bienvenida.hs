c69c9936b85b1f36a152d01025bb7e15c6b2a5fd
Jorgegc294 <a22gucajo@iesgrancapitan.org>
2023-09-28 10:20:18 +0200

    Primeros scrips

---
+++ Bienvenida.hs
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script:  script2
+#descripcion: Mensaje de bienvenida
+#autor:Jorge
+
+clear
+echo:"Bienvenido"
+whoami
---
+++ script3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script:  script3
+#descripcion: Muestra el contenido del directorio actual y la fecha
+#autor:Jorge
+#fecha: 28/09/2023
+
+clear
+echo "Contenido Directorio"
+echo "<___________________>"
+ls
+date
