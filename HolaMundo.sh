55a582e10c4723bd970a01a52f0f13abb3742750
Jorgegc294 <a22gucajo@iesgrancapitan.org>
2023-09-28 08:56:37 +0200

    Script HolaMundo

---
+++ HolaMundo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Saludo en español
+echo "Hola Mundo!"
+
+# Pregunta el nombre del usuario
+read -p "¿Cómo te llamas? " nombre
+
+# Imprime un saludo personalizado
+echo "Encantado de conocerte, $nombre!"
